53f31746c8fab7feade71282ca0b37d53e0864f0
donetian-petkov <donetianpetkov@gmail.com>
2023-09-09 16:43:19 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import WeatherDisplay from './components/WeatherDisplay';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div className="app">
+      <h1>Weather App</h1>
+      <SearchBar />
+      <WeatherDisplay />
     </div>
-  );
-}
+);
 
 export default App;
